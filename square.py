9cdae296feb53a524b91f466fa6fb7ff02818274
EnGentech <engen.inyang@gmail.com>
2023-07-15 11:45:00 -0700

    ok

---
+++ square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Improvement for Square"""
+class Square():
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        p = 0
+        for x in range(4):
+            p = p + self.width
+        return p
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
